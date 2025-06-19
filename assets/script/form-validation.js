c25489845b71ab9a8e4e4f6f880f1ffed8b84819
alam-umair <umair20nov1@gmail.com>
2025-06-19 13:27:12 +0530

    form data is now sending properly and mobile design is complete

--- assets/script/form-validation.js
+++ assets/script/form-validation.js
@@ -1,9 +1,11 @@
 // below code is for the form validation functionality
 document
   .querySelector(".contact-form")
-  .addEventListener("submit", function (event) {
+  .addEventListener("submit", async function (event) {
     event.preventDefault(); // Prevent the default form submission behavior
 
+    const form = event.target;
+    // console.log(form);
     const firstName = document.getElementById("first-name").value;
     const lastName = document.getElementById("last-name").value;
     const email = document.getElementById("email").value;
@@ -12,7 +14,6 @@ document
     const generalRad = document.querySelector("#general").checked;
     const supportRad = document.querySelector("#support").checked;
 
-    // console.log("first name:",firstName,"lastName:",lastName,"email:",email,"message:",message,"checkbox:",checkbox);
     function toggleError(element, condition) {
       document.querySelector(element).style.display = condition
         ? "block"
@@ -34,10 +35,52 @@ document
       (generalRad || supportRad);
 
     if (isFormValid) {
-      document.querySelector(".message-container").style.display = "flex";
-      document.querySelector(".contact-form").reset(); // optional reset
+      // Send form data to Formspree using fetch
+      const formData = new FormData(form);
+      try {
+        const response = await fetch(form.action, {
+          method: "POST",
+          body: formData,
+          headers: {
+            Accept: "application/json",
+          },
+        });
+        // console.log("response: ", response.status);
+        if (response.ok) {
+          // Show your custom message and reset the form
+          document.querySelector(".message-container").style.display = "flex";
+          form.reset();
+        } else {
+          const data = await response.json();
+          alert(
+            "Error: " +
+              (data.error ||
+                "There was an error submitting the form. Please try again laters.")
+          );
+        }
+      } catch (err) {
+        alert(
+          "There was an error submitting the form. Please try again later."
+        );
+        console.log(err);
+      }
     }
   });
+
+const radioButtons = document.querySelectorAll('input[name="query-type"]');
+const radioContainer = document.querySelectorAll(".radio");
+
+radioButtons.forEach((radio) => {
+  radio.addEventListener("change", () => {
+    radioContainer.forEach((container) => {
+      container.classList.remove("selected");
+    });
+    const selectedRadio = radio.closest(".radio");
+    if (selectedRadio) {
+      selectedRadio.classList.add("selected");
+    }
+  });
+});
 
 function closePopup() {
   document.querySelector(".message-container").style.display = "none";
